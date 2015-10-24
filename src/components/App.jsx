6fa86e8114b08c80938d2b0b8988c6a00bed4132
OBellon <OscarBellon96@hotmail.com>
2015-10-24 22:52:20 +0200

    Testing more

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,8 +2,18 @@ import React from 'react';
 
 export default React.createClass({
   render: function() {
-    return <div>
-      <a className="btn btn-primary" href="#">Hi World!</a>
-    </div>
+    return (
+    		<Main/>
+    );
+  }
+});
+
+var Main = React.createClass({ 
+  render: function(){
+  	return (
+  		<div>
+      		<a className="btn btn-danger" href="#">Hi World!</a>
+    	</div>
+    );
   }
 });
